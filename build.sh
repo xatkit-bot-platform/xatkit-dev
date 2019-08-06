fe6ae0e3b3de9cb2908cd5e383097b76fc22e25a
Gwendal <gwendal.daniel@gmail.com>
2019-08-06 15:11:26 +0200

    add --eclipse parameter to build eclipse update site

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 mvn_options=""
 build_product=false
+build_eclipse=false
 
 for arg in "$@"
 do
@@ -9,6 +10,7 @@ do
 	case "$arg" in
 		"--skip-tests") 	mvn_options="$mvn_options -DskipTests" ;;
 		"--product")		mvn_options="$mvn_options -Pbuild-product"; build_product=true ;;
+		"--eclipse")		build_eclipse=true;;
 		*) 					echo "Unknown argument $arg"; exit 1
 	esac
 done
@@ -36,7 +38,7 @@ cd $XATKIT_DEV/src/xatkit
 
 echo "Pulling Xatkit"
 git pull
-echo "Building xatkit"
+echo "Building Xatkit"
 mvn clean install $mvn_options
 mvn_result=$?
 if [ $mvn_result == 0 ]
@@ -47,8 +49,31 @@ then
 		cp core/target/xatkit-nodep-jar-with-dependencies.jar $XATKIT_DEV/build/bin
 	fi
 else
-	echo "An error occured when building xatkit, see the maven build log"
+	echo "An error occurred when building xatkit, see the maven build log"
 	exit 1
+fi
+
+if [ $build_eclipse = true ]
+then
+	if [ -d $XATKIT_DEV/update-site ]
+	then
+		rm -rf $XATKIT_DEV/update-site
+	fi
+
+	mkdir -p $XATKIT_DEV/update-site
+
+	cd $XATKIT_DEV/src/xatkit/eclipse
+	echo "Building Xatkit Eclipse Plugins"
+	mvn clean install $mvn_options
+	mvn_result=$?
+	if [ $mvn_result == 0 ]
+	then
+		echo "Copying update site"
+		cp update/com.xatkit.update/target/com.xatkit.update-2.0.0-SNAPSHOT.zip $XATKIT_DEV/update-site
+	else
+		echo "An error occurred wen building Xatkit Eclipse Plugins, see the maven build log"
+		exit 1
+	fi
 fi
 
 cd $XATKIT_DEV/src/platforms
