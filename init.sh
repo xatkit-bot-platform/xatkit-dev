50fff9a3bb111fda4c174cee6253bea2f0c466a7
Gwendal <gwendal.daniel@gmail.com>
2019-09-15 21:55:52 +0200

    init.sh now skip existing directories instead of failing
    This will be useful to update the XDK while keeping existing local directories safe

--- init.sh
+++ init.sh
@@ -15,62 +15,70 @@ cd $XATKIT_DEV
 
 if [ -d $XATKIT_DEV/src/xatkit-runtime ]
 then
-	echo "Cannot initialize Xatkit development toolkit, there are already a xatkit-runtime directory in your development environment ($XATKIT/src/xatkit-runtime)"
-	exit 1
-fi
+	echo "Skipping initialization of Xatkit Runtime, there is already a xatkit-runtime directory in your development environment ($XATKIT_DEV/src/xatkit-runtime)"
+else
+	cd $XATKIT_DEV/src
 
-cd $XATKIT_DEV/src
+	echo "Cloning Xatkit"
+	git clone $xatkit_org/xatkit-runtime.git
 
-echo "Cloning Xatkit"
-git clone $xatkit_org/xatkit-runtime.git
-
-if [ $? -ne 0 ]
-then
-	echo "Cannot clone $xatkit_org/xatkit.git"
-	exit 1
+	if [ $? -ne 0 ]
+	then
+		echo "Cannot clone $xatkit_org/xatkit.git"
+		exit 1
+	fi
 fi
 
 if [ -d $XATKIT_DEV/src/xatkit-eclipse ]
 then
-	echo "Cannot initialize Xatkit development toolkit, there are already a xatkit-eclipse directory in your development environment ($XATKIT/src/xatkit-eclipse)"
-	exit 1
+	echo "Skipping initialization of Xatkit Eclipse, there is already a xatkit-eclipse directory in your development environment ($XATKIT_DEV/src/xatkit-eclipse)"
+else 
+	echo "Cloning Xatkit Eclipse Plugins"
+	git clone $xatkit_org/xatkit-eclipse.git
+
+	if [ $? -ne 0 ]
+	then
+		echo "Cannot clone $xatkit_org/xatkit-eclipse.git"
+		exit 1
+	fi
 fi
 
-echo "Cloning Xatkit Eclipse Plugins"
-git clone $xatkit_org/xatkit-eclipse.git
-
-if [ $? -ne 0 ]
+if [ -d $XATKIT_DEV/src/xatkit-examples ]
 then
-	echo "Cannot clone $xatkit_org/xatkit-eclipse.git"
-	exit 1
+	echo "Skipping initialization of Xatkit Examples, there is already a xatkit-examples directory in your development environment ($XATKIT_DEV/src/xatkit-examples)"
+else
+	echo "Cloning Xatkit Examples"
+	git clone $xatkit_org/xatkit-examples
+
+	if [ $? -ne 0 ]
+	then
+		echo "Cannot clone $xatkit_org/xatkit-examples"
+		exit 1
+	fi
 fi
 
-if [ -d $XATKIT_DEV/src/platforms ]
+if [ ! -d $XATKIT_DEV/src/platforms ]
 then
-	echo "Cannot initialize Xatkit development toolkit, there are already platform directories in your development environment ($XATKIT_DEV/src/platforms)"
-	exit 1
+	echo "Creating $XATKIT_DEV/src/platforms directory"
+	mkdir -p $XATKIT_DEV/src/platforms
 fi
-
-echo "Cloning Xatkit Examples"
-git clone $xatkit_org/xatkit-examples
-
-if [ $? -ne 0 ]
-then
-	echo "Cannot clone $xatkit_org/xatkit-examples"
-	exit 1
-fi
-
-mkdir -p $XATKIT_DEV/src/platforms
 
 cd $XATKIT_DEV/src/platforms
+
+echo "Initializing platforms"
 
 for platform in "${embedded_platforms[@]}"
 do
 	project_name="$platform-platform"
-	echo "Cloning $project_name"
-	git clone $xatkit_org/$project_name.git
-	if [ $? -ne 0 ]
+	if [ -d $XATKIT_DEV/src/platforms/$project_name ]
 	then
-		echo "Cannot clone $xatkit_org/$project_name.git"
+		echo "Skipping initialization of $project_name, the directory already exists"
+	else 
+		echo "Cloning $project_name"
+		git clone $xatkit_org/$project_name.git
+		if [ $? -ne 0 ]
+		then
+			echo "Cannot clone $xatkit_org/$project_name.git"
+		fi
 	fi
 done
